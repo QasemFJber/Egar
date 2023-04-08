774fcef671ca5823e19c081e07422838f7858a35
jaber <jaberqasem75@gmail.com>
2023-04-08 13:28:16 +0200

    Commit Today

--- app/src/main/java/com/example/egar/Activities/Splash.java
+++ app/src/main/java/com/example/egar/Activities/Splash.java
@@ -17,7 +17,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),FollowOrder.class);
+                Intent intent = new Intent(getApplicationContext(),Login.class);
                 startActivity(intent);
 
             }
