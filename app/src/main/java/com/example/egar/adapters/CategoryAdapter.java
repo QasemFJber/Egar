50c98f22e2c89cb2343351c8b4640395161c6b64
jaber <jaberqasem75@gmail.com>
2023-05-01 23:02:56 +0300

    .

--- app/src/main/java/com/example/egar/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/egar/adapters/CategoryAdapter.java
@@ -12,49 +12,65 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.egar.Models.Category;
 import com.example.egar.R;
 import com.example.egar.databinding.CustomCategoryItemBinding;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+import java.util.List;
 
-    private Context context ;
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
 
-    private String name;
-    private  int image ;
+    private List<Category> categoryList;
+    private OnItemClickListener listener;
 
-    public CategoryAdapter(Context context, String name, int image) {
-        this.context = context;
-        this.name = name;
-        this.image = image;
+    public interface OnItemClickListener {
+        void onItemClick(Category category);
+    }
+
+    public CategoryAdapter(List<Category> categoryList, OnItemClickListener listener) {
+        this.categoryList = categoryList;
+        this.listener = listener;
+    }
+
+    // ViewHolder class to hold each item in the RecyclerView
+    public static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView;
+        ImageView imageView;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.category_title);
+            imageView = itemView.findViewById(R.id.category_image);
+        }
+
+        public void bind(final Category category, final OnItemClickListener listener) {
+            nameTextView.setText(category.getName());
+            imageView.setImageDrawable(category.getImageUrl());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(category);
+                }
+            });
+        }
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_category_item,parent,false);
-        return new ViewHolder(view);
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_category_item, parent, false);
+        CategoryViewHolder viewHolder = new CategoryViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-//        holder.binding.textView5.setText();
-
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        Category category = categoryList.get(position);
+        holder.bind(category, listener);
     }
 
     @Override
     public int getItemCount() {
-        return 10;
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder{
-        CustomCategoryItemBinding binding;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(view -> {
-                Toast.makeText(context.getApplicationContext(), "welcome ", Toast.LENGTH_SHORT).show();
-            });
-
-        }
+        return categoryList.size();
     }
 }
