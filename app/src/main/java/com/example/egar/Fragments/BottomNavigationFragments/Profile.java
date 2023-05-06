f4d678a8af389f1aca24895972774d5fe7ad0a48
jaber <jaberqasem75@gmail.com>
2023-05-06 23:51:43 +0300

    .

--- app/src/main/java/com/example/egar/Fragments/BottomNavigationFragments/Profile.java
+++ app/src/main/java/com/example/egar/Fragments/BottomNavigationFragments/Profile.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.egar.R;
+import com.example.egar.databinding.FragmentHomeBinding;
+import com.example.egar.databinding.FragmentProfileBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Profile#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Profile extends Fragment {
+public class Profile extends Fragment  implements View.OnClickListener {
+    private FragmentProfileBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +64,23 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding  = FragmentProfileBinding.inflate(inflater,container,false);
+        setOnClick();
+        return binding.getRoot();
+    }
+
+    private void  setOnClick(){
+        binding.btnUpdate.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_update:
+                binding.tvInformation.setEnabled(true);
+                binding.tvConversations.setEnabled(true);
+                binding.tvLanguage.setEnabled(true);
+                break;
+        }
     }
 }
