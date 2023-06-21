9c77e2a5377ed6e0b608c78b9fcb1dbd02da6522
jaber <jaberqasem75@gmail.com>
2023-06-21 23:13:12 +0300

    Load Data From Firebase

---
+++ app/src/main/java/com/example/egar/Models/Offer.java
@@ -0,0 +1,68 @@
+package com.example.egar.Models;
+
+public class Offer {
+    private String id;
+    private Product product;
+    private double price;
+    private int quantity;
+    private String startDate;
+    private String endDate;
+
+
+
+    public Offer(Product product, double price, int quantity, String startDate, String endDate) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
