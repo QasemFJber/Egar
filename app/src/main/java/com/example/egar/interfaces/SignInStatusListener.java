4755319aa2ceacb601bbf9ffc82cd4ca5eb13168
jaber <jaberqasem75@gmail.com>
2023-06-21 23:13:27 +0300

    Load Data From Firebase

--- app/src/main/java/com/example/egar/interfaces/SignInStatusListener.java
+++ app/src/main/java/com/example/egar/interfaces/SignInStatusListener.java
@@ -3,6 +3,6 @@ package com.example.egar.interfaces;
 public interface SignInStatusListener {
     void onUserSignedInAsRegularUser(String id);
     void onUserSignedInAsAdminUser(String id);
-    void onUserNotSignedIn();
+    void onUserNotSignedIn(String uid);
 }
 
