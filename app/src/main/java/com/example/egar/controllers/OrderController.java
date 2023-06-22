c2e0eaca3d2199087ad2fcc83a65d9ebfba6b4d2
jaber <jaberqasem75@gmail.com>
2023-06-22 15:35:59 +0300

    Load Data From Firebase

--- app/src/main/java/com/example/egar/controllers/OrderController.java
+++ app/src/main/java/com/example/egar/controllers/OrderController.java
@@ -6,13 +6,19 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.example.egar.Models.Order;
+import com.example.egar.interfaces.OnOrderFetchListener;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class OrderController {
 
@@ -34,57 +40,58 @@ public class OrderController {
         return instance;
     }
 
-    public void addOrder(Order order) {
-        // Add order to Firestore
-        // Get a reference to the orders collection in Firestore
-        CollectionReference ordersCollection = FirebaseFirestore.getInstance().collection("orders");
+    public void addOrder(Order order, OnOrderFetchListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference ordersCollection = db.collection("orders");
 
-        // Convert order object to a HashMap
         HashMap<String, Object> orderData = new HashMap<>();
-        orderData.put("orderId", order.getOrderId());
-        orderData.put("customerId", order.getCustomerId());
+
+        orderData.put("userId", order.getUserId());
         orderData.put("serviceProviderId", order.getServiceProviderId());
         orderData.put("quantity", order.getQuantity());
-        orderData.put("totalPrice", order.getTotalAmount());
+        orderData.put("totalAmount", order.getTotalAmount());
         orderData.put("orderDate", order.getOrderDate());
+        orderData.put("orderStatus", order.getOrderStatus().toString());
+        orderData.put("paymentMethod", order.getPaymentMethod());
+        orderData.put("shippingLocation", order.getShippingLocation());
 
-        // Add the order to Firestore
         ordersCollection.add(orderData)
                 .addOnSuccessListener(documentReference -> {
-                    // On success, log a message to the console
+                    orderData.put("orderId", documentReference.getId());
                     Log.d(TAG, "Order added with ID: " + documentReference.getId());
+                    listener.onAddOrderSuccess(documentReference.getId());
                 })
                 .addOnFailureListener(e -> {
                     // On failure, log an error message to the console
                     Log.e(TAG, "Error adding order", e);
+                    listener.onAddOrderFailure(e.getMessage());
                 });
     }
 
-    public void updateOrder(Order order) {
-        // Update order in Firestore
+    public void updateOrder(Order order,OnOrderFetchListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference orderRef = db.collection("orders").document(order.getOrderId());
 
-        orderRef.update("customerId", order.getCustomerId(),
+        orderRef.update("userId", order.getUserId(),
                         "serviceProviderId", order.getServiceProviderId(),
                         "quantity", order.getQuantity(),
-                        "totalPrice", order.getTotalAmount(),
-                        "orderDate", order.getOrderDate())
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "Order updated successfully");
-                    }
+                        "totalAmount", order.getTotalAmount(),
+                        "orderDate", order.getOrderDate(),
+                        "orderStatus", order.getOrderStatus().toString(),
+                        "paymentMethod", order.getPaymentMethod(),
+                        "shippingLocation", order.getShippingLocation())
+                .addOnSuccessListener(aVoid -> {
+                    listener.onDeleteOrderSuccess();
+                    Log.d(TAG, "Order updated successfully");
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error updating order", e);
-                    }
+                .addOnFailureListener(e -> {
+                    Log.w(TAG, "Error updating order", e);
+                    listener.onDeleteOrderFailure(e.getMessage());
                 });
     }
 
-    public void deleteOrder(Order order) {
-        // Delete order from Firestore
+
+    public void deleteOrder(Order order,OnOrderFetchListener listener) {
         db.collection("orders").document(order.getOrderId())
                 .delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -100,5 +107,27 @@ public class OrderController {
                     }
                 });
     }
+    public void getOrdersByServiceProviderId(String serviceProviderId,OnOrderFetchListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference ordersCollection = db.collection("orders");
+
+        Query query = ordersCollection.whereEqualTo("serviceProviderId", serviceProviderId);
+
+        query.get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<Order> orders = new ArrayList<>();
+                        for (DocumentSnapshot document : task.getResult()) {
+                            Order order = document.toObject(Order.class);
+                            orders.add(order);
+                            listener.onGetOrdersByServiceProviderIdSuccess(orders);
+                        }
+                    } else {
+                        Log.w(TAG, "Error getting orders by service provider ID", task.getException());
+                        listener.onGetOrdersByServiceProviderIdFailure(task.getException().getMessage());
+                    }
+                });
+    }
+
 }
 
