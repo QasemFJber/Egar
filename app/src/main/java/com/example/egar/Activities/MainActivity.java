0557ccf3cc36ff420ef11f7d361ce9fa4b85555a
jaber <jaberqasem75@gmail.com>
2023-07-16 09:56:09 +0300

    Load Data From Firebase and change password and fix auth problem

--- app/src/main/java/com/example/egar/Activities/MainActivity.java
+++ app/src/main/java/com/example/egar/Activities/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                 dialog.cancel();
 
             }
-        },6000);
+        },3500);
     }
 
     private  void setOnClick(){
